39f1a677d2328d7a2a5f1a8a75afd4477a8b22f2
Soumendu Majee <soumendu.fb9@gmail.com>
2020-07-16 01:41:05 -0400

    adds requirements and cleans utils

--- demo.py
+++ demo.py
@@ -6,6 +6,30 @@ import numpy as np
 from utils import *
 
 
-fname = '../data/'
+fname = 'data/shepp_slice0001.2Dsinodata'
+print(fname)
 
-y = read_sino_openmbir(fname, '.2Dsinodata', 288, 1, 512):
+# y = read_sino_openmbir(fname, '.2Dsinodata', 288, 1, 512)
+
+sizesArray = (1, 288, 512)
+with open(fname, 'rb') as fileID:
+    numElements = sizesArray[1]*sizesArray[2]
+    x = np.fromfile(fileID, dtype='float32', count=numElements).reshape([sizesArray[1], sizesArray[2]])
+
+print(x.shape)
+
+np.save('data/sinodata.npy', x)
+
+fname = 'data/shepp_slice0001.2Dweightdata'
+print(fname)
+
+# y = read_sino_openmbir(fname, '.2Dweightdata', 288, 1, 512)
+
+sizesArray = (1, 288, 512)
+with open(fname, 'rb') as fileID:
+    numElements = sizesArray[1]*sizesArray[2]
+    x = np.fromfile(fileID, dtype='float32', count=numElements).reshape([sizesArray[1], sizesArray[2]])
+
+print(x.shape)
+
+np.save('data/weightdata.npy', x)
--- utils.py
+++ utils.py
@@ -4,18 +4,16 @@
 
 import os
 import sys
-import argparse
-import random
+from glob import glob
 import numpy as np
 import pdb
-from glob import glob
 # pdb.set_trace()
 
-import matplotlib.pyplot as plt
-from PIL import Image
 from ruamel.yaml import YAML
 
-# ------- params read/modify ------------------
+############################################################################
+## mbir read/modify Param Files
+############################################################################
 
 def read_params(params_path):
 
@@ -74,9 +72,11 @@ def read_sino_openmbir(rootPath, suffix, N_theta, N_z, N_y):
 
 def write_sino_openmbir(x, rootPath, suffix):
 
+    # shape of x = N_theta x N_z  x N_y
+
     assert len(x.shape)==3, 'data must be 3D'
 
-    x = np.copy(np.swapaxes(x, 0, 1), order='C') # shape = N_z x N_theta x N_y
+    x = np.copy(np.swapaxes(x, 0, 1), order='C') 
 
     fname_list = generateFileList(x.shape[0], rootPath, suffix)
 
@@ -102,6 +102,8 @@ def read_recon_openmbir(rootPath, suffix, N_x, N_y, N_z):
 
 def write_recon_openmbir(x, rootPath, suffix):
 
+    # shape of x = N_z x N_y x N_x
+
     assert len(x.shape)==3, 'data must be 3D'
 
     fname_list = generateFileList(x.shape[0], rootPath, suffix)
@@ -111,95 +113,6 @@ def write_recon_openmbir(x, rootPath, suffix):
         with open(fname, 'wb') as fileID:
             x[i].astype('float32').flatten('C').tofile(fileID)
 
-#############################################################################
-## 3D read/write routines
-#############################################################################
-
-def read_recon3D(filePath):
-
-    x = read_ND(filePath, 3, dtype='float32', ntype='int32') # shape = N_x x N_y x N_z
-    x = np.copy(np.swapaxes(x, 0, 2), order='C')
-
-    return x
-
-def write_recon3D(x, filePath):
-
-    assert len(x.shape)==3, 'data must be 3D'
-
-    x = np.copy(np.swapaxes(x, 0, 2), order='C') # shape = N_x x N_y x N_z
-    write_ND(x, filePath, dtype='float32', ntype='int32')
-
-
-def read_sino3D(filePath):
-
-    x = read_ND(filePath, 3, dtype='float32', ntype='int32') # shape = N_theta x N_y x N_z
-    x = np.copy(np.swapaxes(x, 1, 2), order='C')
-
-    return x
-
-def write_sino3D(x, filePath):
-
-    assert len(x.shape)==3, 'data must be 3D'
-
-    x = np.copy(np.swapaxes(x, 1, 2), order='C') # shape = N_theta x N_y x N_z
-    write_ND(x, filePath, dtype='float32', ntype='int32')
-
-
-#############################################################################
-## read/write N-D arrays as binary files
-#############################################################################
-
-
-def read_ND_list(fNameList, n_dim, dtype='float32', ntype='int32'):
-
-    return [read_ND(fName, n_dim, dtype, ntype) for fName in fNameList ]
-
-
-def write_ND_list(dataList, fNameList, order_type='recon'):
-
-    assert len(fNameList)==len(dataList), 'write_ND_list: dimension mismatch'
-
-    for data, fName in zip(dataList,fNameList):
-        write_ND(data, fName, dtype, ntype)
-
-
-def read_ND(filePath, n_dim, dtype='float32', ntype='int32'):
-
-    with open(filePath, 'rb') as fileID:
-
-        sizesArray = np.fromfile( fileID, dtype=ntype, count=n_dim)
-        numElements = np.prod(sizesArray)
-        dataArray = np.fromfile(fileID, dtype=dtype, count=numElements).reshape(sizesArray)
-
-    return dataArray
-
-
-def write_ND(dataArray, filePath, dtype='float32', ntype='int32'):
-
-    sizesArray = np.asarray(dataArray.shape)
-
-    with open(filePath, 'wb') as fileID:
-
-        sizesArray.astype(ntype).flatten('C').tofile(fileID)
-        dataArray.astype(dtype).flatten('C').tofile(fileID)
-
-
-#############################################################################
-## read/write Image Lists
-#############################################################################
-
-def read_stackOfFileNames(dirPathList, file_ext):
-
-    # each directory must have stack of 3D-recon files (4D)
-    stackOfFileNames = []
-    for dirPath in dirPathList:
-
-        wildCardFname = os.path.join(dirPath,'*.'+file_ext)
-        fNameList = glob(wildCardFname)
-        fNameList.sort()
-        stackOfFileNames.append(fNameList)
-
-    return stackOfFileNames
 
 def generateFileList(numFiles, fileRoot, suffix):
 
@@ -209,29 +122,3 @@ def generateFileList(numFiles, fileRoot, suffix):
 
     return fileList
 
-
-def writeFileList(filePath, fileList):
-
-    with open(filePath,'w') as fileID:
-    
-        fileID.write(str(len(fileList) )+"\n")
-        for fileName in fileList:
-            fileID.write(fileName+"\n")
-
-
-def readFileList(filePath, resolvePath=True):
-
-    with open(filePath,'r') as fileID:
-
-        lines = fileID.read().split("\n")
-
-    numLines = int(lines[0])
-    fileList = lines[1:numLines+1]
-
-    if resolvePath==True:
-        origDir = os.path.dirname(filePath)
-        for i in range(len(fileList)):
-            tempPath = os.path.join(origDir, fileList[i])
-            fileList[i] = os.path.abspath( tempPath )
-
-    return fileList
