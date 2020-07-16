ffd4e9dd2f24eebb98b25f1e4addbd4150da1410
Soumendu Majee <soumendu.fb9@gmail.com>
2020-07-16 00:49:09 -0400

    adds python wrapper and utils

---
+++ svmbir.py
@@ -0,0 +1,158 @@
+
+import os
+import argparse
+from glob import glob
+import numpy as np
+import subprocess
+
+from utils import *
+
+# from multiprocessing import Pool
+# import matplotlib.pyplot as plt
+
+__exec_path__ = os.path.realpath(os.path.join(os.path.dirname(__file__),'sv-mbirct/bin/mbir_ct'))
+
+def cmd_exec(exec_path=__exec_path__, *args, **kwargs):
+
+    arg_list = [exec_path]
+    for key in args:
+        arg_list.append('-'+key)
+
+    for key,value in kwargs.items():
+        arg_list.append('-'+key)
+        arg_list.append(value)
+
+    # print(arg_list)
+    # os.environ['OMP_NUM_THREADS'] = '20'
+    # os.environ['OMP_DYNAMIC'] = 'true'
+    subprocess.run(arg_list)
+
+
+def gen_paths(mbir_data_path, mbir_params_path, object_name):
+
+    paths = dict()
+
+    paths['param_name'] = os.path.join(mbir_params_path,object_name)
+    paths['sino_name'] = os.path.join(mbir_data_path,'sino',object_name)
+    paths['wght_name'] = os.path.join(mbir_data_path,'weight',object_name)
+    paths['recon_name'] = os.path.join(mbir_data_path,'recon',object_name)
+    paths['init_name'] = os.path.join(mbir_data_path,'init',object_name)
+    paths['proj_name'] = os.path.join(mbir_data_path,'proj',object_name)
+    paths['sysmatrix_name'] = os.path.join(mbir_data_path,'sysmatrix',object_name)
+
+    return paths
+
+
+def init_geometry_data(mbir_data_path, mbir_params_path, object_name, angles, **sinoparams):
+
+    paths = gen_paths(mbir_data_path, mbir_params_path, object_name)
+
+    sinoparams_req_keys = ['NChannels', 'NViews', 'NSlices', 'CenterOffset']
+
+    assert all(key in sinoparams.keys() for key in sinoparams_req_keys), 'required keys ({}) not present'.format(sinoparams_req_keys)
+
+    sinoparams['FirstSliceNumber'] = 0
+    sinoparams['DeltaChannel'] = 1
+    sinoparams['DeltaSlice'] = 1
+
+    modify_params(paths['param_name']+'.sinoparams', **sinoparams)
+
+    imgparams = dict()
+    imgparams['Nx'] = sinoparams['NChannels']
+    imgparams['Ny'] = sinoparams['NChannels']
+    imgparams['Nz'] = sinoparams['NSlices']
+    imgparams['FirstSliceNumber'] = 0
+    imgparams['Deltaxy'] = 1
+    imgparams['DeltaZ'] = 1
+    imgparams['ROIRadius'] = sinoparams['NChannels']/2
+
+    modify_params(paths['param_name']+'.imgparams', **imgparams)
+
+    ViewAngleList_fname = paths['param_name']+'.ViewAngleList'
+    with open(ViewAngleList_fname,'w') as fileID:
+        for angle in list(angles):
+            fileID.write(str(angle)+"\n")
+
+
+def gen_sysmatrix(mbir_data_path, mbir_params_path, object_name):
+
+    paths = gen_paths(mbir_data_path, mbir_params_path, object_name)
+
+    if not os.path.exists(os.path.dirname(paths['sysmatrix_name'])):
+        os.makedirs(os.path.dirname(paths['sysmatrix_name']), exist_ok=True)
+
+    cmd_exec(i=paths['param_name'], j=paths['param_name'], m=paths['sysmatrix_name'])
+
+
+def recon(mbir_data_path, mbir_params_path, object_name, sino=None, wght=None, init_recon=None, **reconparams):
+
+    paths = gen_paths(mbir_data_path, mbir_params_path, object_name)
+
+    if not os.path.exists(os.path.dirname(paths['recon_name'])):
+        os.makedirs(os.path.dirname(paths['recon_name']), exist_ok=True)
+
+    if not os.path.exists(os.path.dirname(paths['init_name'])):
+        os.makedirs(os.path.dirname(paths['init_name']), exist_ok=True)
+
+    if not os.path.exists(os.path.dirname(paths['proj_name'])):
+        os.makedirs(os.path.dirname(paths['proj_name']), exist_ok=True)
+
+    if not os.path.exists(os.path.dirname(paths['sino_name'])):
+        os.makedirs(os.path.dirname(paths['sino_name']), exist_ok=True)
+
+    if not os.path.exists(os.path.dirname(paths['wght_name'])):
+        os.makedirs(os.path.dirname(paths['wght_name']), exist_ok=True)
+
+    modify_params(paths['param_name']+'.reconparams', **reconparams)
+    
+    if sino is not None:
+        write_sino_openmbir(sino, paths['sino_name']+'_slice', '.2Dsinodata')
+    if wght is not None:
+        write_sino_openmbir(wght, paths['wght_name']+'_slice', '.2Dweightdata')
+    
+    if init_recon is not None:
+        write_recon_openmbir(init_recon, paths['init_name']+'_slice', '.2Dimgdata')
+
+        print('Starting with initial recon')
+
+        cmd_exec(i=paths['param_name'], j=paths['param_name'], k=paths['param_name'], 
+            s=paths['sino_name'], w=paths['wght_name'], f=paths['proj_name'],
+            r=paths['recon_name'], t=paths['init_name'],
+            m=paths['sysmatrix_name'])
+
+    else:
+
+        cmd_exec(i=paths['param_name'], j=paths['param_name'], k=paths['param_name'], 
+            s=paths['sino_name'], w=paths['wght_name'], f=paths['proj_name'],
+            r=paths['recon_name'],
+            m=paths['sysmatrix_name'])
+
+    imgparams = read_params(paths['param_name']+'.imgparams')
+    x = read_recon_openmbir(paths['recon_name']+'_slice', '.2Dimgdata', 
+        imgparams['Nx'], imgparams['Ny'], imgparams['Nz'])
+
+    return x
+
+
+def project(mbir_data_path, mbir_params_path, object_name, recon=None):
+
+    paths = gen_paths(mbir_data_path, mbir_params_path, object_name)
+
+    if not os.path.exists(os.path.dirname(paths['proj_name'])):
+        os.makedirs(os.path.dirname(paths['proj_name']), exist_ok=True)
+
+    if not os.path.exists(os.path.dirname(paths['recon_name'])):
+        os.makedirs(os.path.dirname(paths['recon_name']), exist_ok=True)
+
+
+    if recon is not None:
+        write_recon_openmbir(recon, paths['recon_name']+'_slice', '.2Dimgdata')
+
+    cmd_exec(i=paths['param_name'], j=paths['param_name'], m=paths['sysmatrix_name'],
+        f=paths['proj_name'], t=paths['recon_name'])
+
+    sinoparams = read_params(paths['param_name']+'.sinoparams')
+    p = read_sino_openmbir(paths['proj_name']+'_slice', '.2Dprojection', 
+        sinoparams['NViews'], sinoparams['NSlices'], sinoparams['NChannels'])
+
+    return p
---
+++ utils.py
@@ -0,0 +1,293 @@
+# from __future__ import absolute_import
+# from __future__ import division
+# from __future__ import print_function
+
+import os
+import sys
+import argparse
+import random
+import numpy as np
+import pdb
+from glob import glob
+# pdb.set_trace()
+
+import matplotlib.pyplot as plt
+from PIL import Image
+from ruamel.yaml import YAML
+
+def save_graph(path, variableDict):
+    """
+    saves list of variable names along with their corresponding arrays (or list)
+    """
+    makeDir(path)
+    for varName, var in variableDict.items():
+        np.save(os.path.join(path, varName+'.npy'), np.asarray(var))
+
+
+def save_proj(path, proj, baseName='proj'):
+    """
+    saves stack of radiograph and stack of sinograms
+    assumes proj.shape = N_theta x N_z x N_y
+    """
+    sinoStack = np.swapaxes(proj, 0, 1)
+    makeDir(path)
+    np.save(os.path.join(path, baseName+'.sino.npy'), sinoStack)
+
+
+def save_recon(path, recon, baseName='recon'):
+    """
+    saves stack of slices
+    assumes recon.shape = N_z x N_y x N_x
+    """
+    makeDir(path)
+    np.save(os.path.join(path, baseName+'.recon.npy'), recon)
+
+
+def normalize_data(data, normType='perc', limits=(0,100)):
+    
+    if normType=='perc':
+        absLim_lo = np.percentile(data, limits[0], axis=None)
+        absLim_hi = np.percentile(data, limits[1], axis=None)
+    elif normType=='abs':
+        absLim_lo = limits[0]
+        absLim_hi = limits[1]
+    else:
+        raise Exception('normalize_data: normType({}) undefined'.format(normType))
+
+    if absLim_hi==absLim_lo:
+        data_normalized = data
+    else:
+        data_normalized = (data-absLim_lo)/(absLim_hi-absLim_lo)
+
+    return data_normalized, absLim_lo, absLim_hi
+    
+
+def makeDirInPath(path):
+    dirName = os.path.dirname(path)
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+
+def makeDir(dirName):
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+
+# ------- params read/modify ------------------
+
+def read_params(params_path):
+
+    with open(params_path, 'r') as fileID:
+        yaml = YAML()
+        params = yaml.load(fileID)
+        
+    return params
+
+
+def print_params(params, start_str=''):
+
+    for key,value in params.items():
+        if isinstance(value, dict):
+            print('{}:'.format(key))
+            print_params(value, start_str='    ')
+        else:
+            print(start_str+'{}: {}'.format(key,value))
+
+def modify_params(filePath, **kwargs):
+    
+    with open(filePath, 'r') as fileID:
+        yaml = YAML()
+        yaml_dict = yaml.load(fileID)
+
+    # print(kwargs.keys())
+
+    for key in kwargs.keys():
+        yaml_dict[key] = kwargs[key]
+
+    with open(filePath, 'w') as fileID:
+        yaml.dump(yaml_dict, fileID)
+
+
+############################################################################
+## mbir read/write Binary Files
+############################################################################
+
+def read_sino_openmbir(rootPath, suffix, N_theta, N_z, N_y):
+
+    fname_list = generateFileList(N_z, rootPath, suffix)
+
+    sizesArray = (N_z, N_theta, N_y)
+    x = np.zeros(sizesArray) 
+
+    for i, fname in enumerate(fname_list):
+
+        with open(fname, 'rb') as fileID:
+            numElements = sizesArray[1]*sizesArray[2]
+            x[i] = np.fromfile(fileID, dtype='float32', count=numElements).reshape([sizesArray[1], sizesArray[2]])
+
+    # shape = N_z x N_theta x N_y
+    x = np.copy(np.swapaxes(x, 0, 1), order='C')
+
+    return x
+
+def write_sino_openmbir(x, rootPath, suffix):
+
+    assert len(x.shape)==3, 'data must be 3D'
+
+    x = np.copy(np.swapaxes(x, 0, 1), order='C') # shape = N_z x N_theta x N_y
+
+    fname_list = generateFileList(x.shape[0], rootPath, suffix)
+
+    for i, fname in enumerate(fname_list):
+
+        with open(fname, 'wb') as fileID:
+            x[i].astype('float32').flatten('C').tofile(fileID)
+
+def read_recon_openmbir(rootPath, suffix, N_x, N_y, N_z):
+
+    fname_list = generateFileList(N_z, rootPath, suffix)
+
+    sizesArray = (N_z, N_y, N_x)
+    x = np.zeros(sizesArray)
+
+    for i, fname in enumerate(fname_list):
+
+        with open(fname, 'rb') as fileID:
+            numElements = sizesArray[1]*sizesArray[2]
+            x[i] = np.fromfile(fileID, dtype='float32', count=numElements).reshape([sizesArray[1], sizesArray[2]])
+
+    return x
+
+def write_recon_openmbir(x, rootPath, suffix):
+
+    assert len(x.shape)==3, 'data must be 3D'
+
+    fname_list = generateFileList(x.shape[0], rootPath, suffix)
+
+    for i, fname in enumerate(fname_list):
+
+        with open(fname, 'wb') as fileID:
+            x[i].astype('float32').flatten('C').tofile(fileID)
+
+#############################################################################
+## 3D read/write routines
+#############################################################################
+
+def read_recon3D(filePath):
+
+    x = read_ND(filePath, 3, dtype='float32', ntype='int32') # shape = N_x x N_y x N_z
+    x = np.copy(np.swapaxes(x, 0, 2), order='C')
+
+    return x
+
+def write_recon3D(x, filePath):
+
+    assert len(x.shape)==3, 'data must be 3D'
+
+    x = np.copy(np.swapaxes(x, 0, 2), order='C') # shape = N_x x N_y x N_z
+    write_ND(x, filePath, dtype='float32', ntype='int32')
+
+
+def read_sino3D(filePath):
+
+    x = read_ND(filePath, 3, dtype='float32', ntype='int32') # shape = N_theta x N_y x N_z
+    x = np.copy(np.swapaxes(x, 1, 2), order='C')
+
+    return x
+
+def write_sino3D(x, filePath):
+
+    assert len(x.shape)==3, 'data must be 3D'
+
+    x = np.copy(np.swapaxes(x, 1, 2), order='C') # shape = N_theta x N_y x N_z
+    write_ND(x, filePath, dtype='float32', ntype='int32')
+
+
+#############################################################################
+## read/write N-D arrays as binary files
+#############################################################################
+
+
+def read_ND_list(fNameList, n_dim, dtype='float32', ntype='int32'):
+
+    return [read_ND(fName, n_dim, dtype, ntype) for fName in fNameList ]
+
+
+def write_ND_list(dataList, fNameList, order_type='recon'):
+
+    assert len(fNameList)==len(dataList), 'write_ND_list: dimension mismatch'
+
+    for data, fName in zip(dataList,fNameList):
+        write_ND(data, fName, dtype, ntype)
+
+
+def read_ND(filePath, n_dim, dtype='float32', ntype='int32'):
+
+    with open(filePath, 'rb') as fileID:
+
+        sizesArray = np.fromfile( fileID, dtype=ntype, count=n_dim)
+        numElements = np.prod(sizesArray)
+        dataArray = np.fromfile(fileID, dtype=dtype, count=numElements).reshape(sizesArray)
+
+    return dataArray
+
+
+def write_ND(dataArray, filePath, dtype='float32', ntype='int32'):
+
+    sizesArray = np.asarray(dataArray.shape)
+
+    with open(filePath, 'wb') as fileID:
+
+        sizesArray.astype(ntype).flatten('C').tofile(fileID)
+        dataArray.astype(dtype).flatten('C').tofile(fileID)
+
+
+#############################################################################
+## read/write Image Lists
+#############################################################################
+
+def read_stackOfFileNames(dirPathList, file_ext):
+
+    # each directory must have stack of 3D-recon files (4D)
+    stackOfFileNames = []
+    for dirPath in dirPathList:
+
+        wildCardFname = os.path.join(dirPath,'*.'+file_ext)
+        fNameList = glob(wildCardFname)
+        fNameList.sort()
+        stackOfFileNames.append(fNameList)
+
+    return stackOfFileNames
+
+def generateFileList(numFiles, fileRoot, suffix):
+
+    fileList = []
+    for i in range(numFiles):
+        fileList.append(fileRoot+str(i)+suffix)
+
+    return fileList
+
+
+def writeFileList(filePath, fileList):
+
+    with open(filePath,'w') as fileID:
+    
+        fileID.write(str(len(fileList) )+"\n")
+        for fileName in fileList:
+            fileID.write(fileName+"\n")
+
+
+def readFileList(filePath, resolvePath=True):
+
+    with open(filePath,'r') as fileID:
+
+        lines = fileID.read().split("\n")
+
+    numLines = int(lines[0])
+    fileList = lines[1:numLines+1]
+
+    if resolvePath==True:
+        origDir = os.path.dirname(filePath)
+        for i in range(len(fileList)):
+            tempPath = os.path.join(origDir, fileList[i])
+            fileList[i] = os.path.abspath( tempPath )
+
+    return fileList
