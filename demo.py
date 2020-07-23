7012253dfb100005435f922d229596726891cd82
Soumendu Majee <soumendu.fb9@gmail.com>
2020-07-23 18:01:41 -0400

    rearranges demo

--- demo.py
+++ demo.py
@@ -11,26 +11,25 @@ weight = np.load('data/weightdata.npy')
 (NViews, NSlices, NChannels) = sino.shape
 angles = np.linspace(0, np.pi, NViews, endpoint=False)
 
-x = svmbir.recon(angles, sino, weight, 
-    num_threads=20,
+x = svmbir.recon(angles, sino, weight, num_threads=20,
     CenterOffset=-6, img_downsamp=4,
     SigmaX=0.6350, T=0.000478, MaxIterations=10)
 
 proj = svmbir.project(angles, x, num_threads=20,
     CenterOffset=-6, img_downsamp=4)
 
-# display output
+
+## display output #################################
 imgplot = plt.imshow(x[0])
 imgplot.set_cmap('gray')
 plt.colorbar()
 plt.savefig('data/recon.png')
 plt.close()
 
-
 imgplot = plt.imshow(np.swapaxes(proj, 0, 1)[0])
 imgplot.set_cmap('gray')
 plt.colorbar()
-plt.savefig('data/proj.png')
+plt.savefig('data/sino.png')
 plt.close()
 
 imgplot = plt.imshow(np.swapaxes(sino-proj, 0, 1)[0])
