c94a7060c9811539c416e800ce4b347eb207506f
Soumendu Majee <soumendu.fb9@gmail.com>
2020-08-06 16:26:11 -0400

    cleans setup.py

--- setup.py
+++ setup.py
@@ -10,20 +10,19 @@ with open("README.md", "r") as fh:
 #                     sources=sources,
 #                     extra_compile_args=['-fopenmp', '-O3'], 
 #                     extra_link_args=['-fopenmp'])
-# mbir_ct = Extension('svmbir/sv-mbirct/bin/mbir_ct')
 
 setup(
-   name='svmbir',
-   version='1.0',
-   description="Python code for fast parallel-beam MBIR (Model Based Iterative Reconstruction) ",
-   long_description=long_description,
-   long_description_content_type="text/markdown",
-   author='Soumendu Majee',
-   author_email='smajee@purdue.edu',
-   packages=['svmbir'], 
-   python_requires='>=3.6',
-   install_requires=['numpy', 'ruamel.yaml', 'matplotlib'], #external packages as dependencies
-   # ext_modules=[mbir_ct]
-  package_data={'svmbir': ['sv-mbirct/bin/mbir_ct']}
+     name='svmbir',
+     version='1.0',
+     description="Python code for fast parallel-beam MBIR (Model Based Iterative Reconstruction) ",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     author='Soumendu Majee',
+     author_email='smajee@purdue.edu',
+     packages=['svmbir'], 
+     python_requires='>=3.6',
+     install_requires=['numpy', 'ruamel.yaml', 'matplotlib'], #external packages as dependencies
+     package_data={'svmbir': ['sv-mbirct/bin/mbir_ct']}
+     # ext_modules=[mbir_ct]
 )
 
