4ec1e1ed257a5e71f6b513da03b5bd269f5fe032
Soumendu Majee <soumendu.fb9@gmail.com>
2020-10-16 16:36:05 -0400

    add comment about center_offset in demo

--- demo/demo_simple.py
+++ demo/demo_simple.py
@@ -10,6 +10,7 @@ sino = np.load('sinodata.npy')
 (num_views, num_slices, num_channels) = sino.shape
 angles = np.linspace(0, np.pi, num_views, endpoint=False)
 
+# center_offset accounts for the center of rotation being offset from the center of the detector by 6 detector channels
 x = svmbir.recon(sino, angles, center_offset=-6)
 
 x_sharp = svmbir.recon(sino, angles, sharpen=2.0, center_offset=-6)
