9dc6d8fe6f202ab700127b9a825856a4092da6b1
Soumendu Majee <soumendu.fb9@gmail.com>
2020-10-16 16:05:57 -0400

    improve simple demo by adding sharpening parameter

--- demo/demo_simple.py
+++ demo/demo_simple.py
@@ -10,12 +10,20 @@ sino = np.load('sinodata.npy')
 (num_views, num_slices, num_channels) = sino.shape
 angles = np.linspace(0, np.pi, num_views, endpoint=False)
 
-x = svmbir.recon(sino, angles, snr_db=50.0, center_offset=-6,
-	num_threads=4, max_iterations=20)
+x = svmbir.recon(sino, angles, center_offset=-6)
+
+x_sharp = svmbir.recon(sino, angles, sharpen=2.0, center_offset=-6)
 
 ## display output #################################
 imgplot = plt.imshow(x[0])
 imgplot.set_cmap('gray')
 plt.colorbar()
 plt.savefig('recon.png')
+plt.close()
+
+## display output #################################
+imgplot = plt.imshow(x_sharp[0])
+imgplot.set_cmap('gray')
+plt.colorbar()
+plt.savefig('recon_sharp.png')
 plt.close()
