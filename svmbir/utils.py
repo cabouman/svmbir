25c8f67f7670e10e8decedb816cb892c12a2603d
DamonLee5 <liwr5damon@gmail.com>
2020-11-12 16:07:25 -0500

    Fixed the bug that x and y are defined outside the gen_ellipse function.

--- svmbir/utils.py
+++ svmbir/utils.py
@@ -1,42 +1,46 @@
 import numpy as np
-import matplotlib.pyplot as plt
-def gen_ellipse(x0,y0,a,b,value,pre_img,theta=0):
+
+def gen_ellipse(x0,y0,a,b,value,pre_img,x_min,x_max,y_min,y_max,theta=0,add_value=False):
     """
-    Generate ellipse in 2D plane.
+    Draw ellipse in a 2D plane with range [x_min,x_max,y_min,y_max]. 
+
+    There are two drawing modes in this function. 
+    When add_value = False, this function will replace the ellipse area with given value.
+    When add_value = True, this function will add value to pre_img's pixels which are inside ellipse area.
     
     Args: 
-        x0(float): X-axis coordinate.
-        y0(float): Y-axis coordinate.
+        x0(float): X-axis coordinate of center.
+        y0(float): Y-axis coordinate of center.
         a(float): X-axis radius.
         b(float): Y-axis radius.
         value(float): Value of the ellipse.
         pre_img(float): Plane that the ellipse added to. 
-        theta(float): Angle of rotation, from -PI to PI.
-
+        x_min(float): Minimum coordinate of X-axis.
+        x_max(float): Maximum coordinate of X-axis.
+        y_min(float): Minimum coordinate of Y-axis.
+        y_max(float): Maximum coordinate of Y-axis.
+        theta(float): [Default=0.0] Angle of rotation, from -PI to PI.
+        add_value(bool): [Default=False] If add_value is True, add value to pre_img's pixels which are inside ellipse area.
 
     Return:
         ndarray: 2D array with the same shape of pre_img.
 
-    """    
-    pre_img[((x-x0)*np.cos(theta)+(y-y0)*np.sin(theta))**2/a**2 \
-    +((x-x0)*np.sin(theta)-(y-y0)*np.cos(theta))**2/b**2<=1]=value
-
-    return pre_img
-
-
-if __name__ == '__main__':
-    num_cols=256
-    num_rows=256
-    axis_rows=np.linspace(-1.2,1.2,num_cols)
-    axis_cols=np.linspace(-2.4,2.4,num_rows)
-
+    """
+    num_rows,num_cols=pre_img.shape
+    axis_rows=np.linspace(x_min,x_max,num_cols)
+    axis_cols=np.linspace(y_min,y_max,num_rows)
     x,y = np.meshgrid(axis_rows,axis_cols)
-    out_img=np.zeros((num_rows,num_cols))
-    out_img=gen_ellipse(0,-0.0184,0.8642,2.012,55,out_img)
 
-    imgplot = plt.imshow(out_img)
-    imgplot.set_cmap('gray')
-    plt.colorbar()
-    #plt.savefig('test_gen_ellipse.png')
-    plt.close()
+    if not add_value:    
+        pre_img[((x-x0)*np.cos(theta)+(y-y0)*np.sin(theta))**2/a**2 \
+        +((x-x0)*np.sin(theta)-(y-y0)*np.cos(theta))**2/b**2<=1]=value
+        out_img=pre_img
+    else:
+        temp_img=((x-x0)*np.cos(theta)+(y-y0)*np.sin(theta))**2/a**2 \
+        +((x-x0)*np.sin(theta)-(y-y0)*np.cos(theta))**2/b**2<=1
+        out_img=pre_img+temp_img*value
+
+    return out_img
+
+
 
