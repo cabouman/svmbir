72b10b32b76d9883d36ce1584bd907d500728b58
DamonLee5 <liwr5damon@gmail.com>
2020-11-13 10:43:50 -0500

    Replace image with shepp logan phantom generated by phantom.py

--- demo/demo_simple.py
+++ demo/demo_simple.py
@@ -1,30 +1,37 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
-
 import svmbir
 
+num_rows = 512
+num_cols = num_rows
+num_views = 288
 
-sino = np.load('sinodata.npy')
+image = svmbir.phantom.gen_shepp_logan(num_rows)
+image = np.expand_dims(image, axis=0)
+
+angles = np.linspace(-np.pi / 2.0, np.pi / 2.0, num_views, endpoint=False)
+sino = svmbir.project(angles, image, max(num_rows, num_cols))
 
 (num_views, num_slices, num_channels) = sino.shape
-angles = np.linspace(0, np.pi, num_views, endpoint=False)
 
-# center_offset accounts for the center of rotation being offset from the center of the detector by 6 detector channels
-x = svmbir.recon(sino, angles, center_offset=-6)
+x = svmbir.recon(sino, angles,snr_db=40.0)
 
-x_sharp = svmbir.recon(sino, angles, sharpness=2.0, center_offset=-6)
+x_sharp = svmbir.recon(sino, angles, sharpness=2.0,snr_db=40.0)
 
-## display output #################################
-imgplot = plt.imshow(x[0])
+## display output for recon #################################
+imgplot = plt.imshow(x[0],vmin=0.95,vmax=1.1)
 imgplot.set_cmap('gray')
 plt.colorbar()
 plt.savefig('output/simple_recon.png')
 plt.close()
+rmse_x=np.sqrt(np.linalg.norm(x[0]-image[0])**2/(image.shape[1]*image.shape[2]))
+print('RMSE for recon:', rmse_x)
 
-## display output #################################
-imgplot = plt.imshow(x_sharp[0])
+## display output for recon with sharpness=2.0 #################################
+imgplot = plt.imshow(x_sharp[0],vmin=0.95,vmax=1.1)
 imgplot.set_cmap('gray')
 plt.colorbar()
 plt.savefig('output/simple_recon_sharp.png')
 plt.close()
+rmse_x_sharp=np.sqrt(np.linalg.norm(x_sharp[0]-image[0])**2/(image.shape[1]*image.shape[2]))
+print('RMSE for recon with sharpness=2.0:', rmse_x_sharp)
