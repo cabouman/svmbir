1ab2a72cf94a1f82e3d4fc7c17c23feb9f4e7c3c
Charles Bouman <Charles.Bouman@gmail.com>
2020-11-16 11:06:22 -0500

    Created 3D Shepp Logan Demo

--- demo/demo_3D_shepp_logan.py
+++ demo/demo_3D_shepp_logan.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 
 import svmbir
 
+
 """
-This file demonstrates the generation of a Shepp-Logan phantom followed by sinogram projection and reconstruction using MBIR. 
+This file demonstrates the generation of a 3D Shepp-Logan phantom followed by sinogram projection and reconstruction using MBIR. 
 The phantom, sinogram, and reconstruction are then displayed. 
 """
 
@@ -31,6 +32,8 @@ if __name__ == '__main__':
     # Simulated image parameters
     num_rows = 256
     num_cols = num_rows
+    center_slice = 16
+    num_slices = 2*center_slice
 
     # Simulated sinogram parameters
     num_views = 144
@@ -38,12 +41,11 @@ if __name__ == '__main__':
     # Reconstruction parameters
     T = 0.1
     p = 1.1
-    sharpness = 2.0
+    sharpness = 4.0
     snr_db = 40.0
 
     # Generate phantom with a single slice
-    phantom = svmbir.phantom.gen_shepp_logan(num_rows)
-    phantom = np.expand_dims(phantom, axis=0)
+    phantom = svmbir.phantom.gen_shepp_logan_3d(num_rows,num_slices)
 
     # Generate array of view angles form -180 to 180 degs
     angles = np.linspace(-np.pi / 2.0, np.pi / 2.0, num_views, endpoint=False)
@@ -58,16 +60,13 @@ if __name__ == '__main__':
     recon = svmbir.recon(sino, angles, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
 
     # Compute Normalized Root Mean Squared Error
-    nrmse = svmbir.phantom.nrmse(recon[0], phantom[0])
+    nrmse = svmbir.phantom.nrmse(recon, phantom)
 
     # display phantom
-    plot_result(phantom[0], title='Shepp Logan Phantom', filename='output/shepp_logan_phantom.png', vmin=1.0, vmax=1.1)
-
-    # display sinogram
-    plot_result(np.squeeze(sino), title='Sinogram', filename='output/shepp_logan_sinogram.png')
+    plot_result(phantom[center_slice-2], title='Center Slice of 3D Shepp Logan Phantom', filename='output/3d_shepp_logan_phantom.png', vmin=1.0, vmax=1.1)
 
     # display reconstruction
-    title = f'Reconstruction with NRMSE={nrmse:.3f}.'
-    plot_result(recon[0], title=title, filename='output/shepp_logan_recon.png', vmin=1.0, vmax=1.1)
+    title = f'Center Slice of 3D Recon with NRMSE={nrmse:.3f}.'
+    plot_result(recon[center_slice-2], title=title, filename='output/3d_shepp_logan_recon.png', vmin=1.0, vmax=1.1)
 
-    plt.show()
+    #plt.show()
