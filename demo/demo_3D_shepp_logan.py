2113c2b7dab9cac4359b676bca927e0ae50dd1d0
Charles Bouman <Charles.Bouman@gmail.com>
2020-11-16 19:36:39 -0500

    Add 3D test and fixed bugs in num_cols and num_rows.

--- demo/demo_3D_shepp_logan.py
+++ demo/demo_3D_shepp_logan.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 import svmbir
 
 
@@ -45,10 +44,12 @@ if __name__ == '__main__':
     snr_db = 40.0
 
     # Generate phantom with a single slice
-    phantom = svmbir.phantom.gen_shepp_logan_3d(num_rows,num_slices)
+    phantom = svmbir.phantom.gen_shepp_logan_3d(num_rows,num_cols,num_slices)
+
+    print('############################################ np.shape(phantom) = ', np.shape(phantom) )
 
     # Generate array of view angles form -180 to 180 degs
-    angles = np.linspace(-np.pi / 2.0, np.pi / 2.0, num_views, endpoint=False)
+    angles = np.linspace(-np.pi/2.0, np.pi/2.0, num_views, endpoint=False)
 
     # Generate sinogram by projecting phantom
     sino = svmbir.project(angles, phantom, max(num_rows, num_cols))
@@ -57,7 +58,7 @@ if __name__ == '__main__':
     (num_views, num_slices, num_channels) = sino.shape
 
     # Perform MBIR reconstruction
-    recon = svmbir.recon(sino, angles, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
+    recon = svmbir.recon(sino, angles, num_rows=num_rows, num_cols=num_cols, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
 
     # Compute Normalized Root Mean Squared Error
     nrmse = svmbir.phantom.nrmse(recon, phantom)
