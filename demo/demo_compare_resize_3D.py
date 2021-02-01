c2a0a23bed368058704a74c327ce8ea51083dc7b
Soumendu Majee <soumendu.fb9@gmail.com>
2021-01-31 22:25:26 -0500

    Compare timing and output shape for resizing demo

--- demo/demo_compare_resize_3D.py
+++ demo/demo_compare_resize_3D.py
@@ -7,6 +7,8 @@ import svmbir
 import skimage
 import PIL
 
+import timeit
+
 def PIL_resize_3D(recon, output_shape):
 
     recon_resized_list = []
@@ -41,16 +43,22 @@ img = svmbir.phantom.gen_microscopy_sample_3d(num_rows,num_cols,num_slices)
 output_shape = (100,25)
 # output_shape = (255,64)
 
-
+start = timeit.default_timer()
 img_resized_sk = sk_resize_3D(img, output_shape)
-img_resized_PIL = PIL_resize_3D(img, output_shape)
+time_sk = timeit.default_timer()
 
+start = timeit.default_timer()
+img_resized_PIL = PIL_resize_3D(img, output_shape)
+time_PIL = timeit.default_timer()
 
 plot_image(img[display_slice], title='original', filename='output/3D_resize_original.png', vmin=vmin, vmax=vmax)
 plot_image(img_resized_sk[display_slice], title='img_resized_sk', filename='output/3D_resize_img_sk.png', vmin=vmin, vmax=vmax)
 plot_image(img_resized_PIL[display_slice], title='img_resized_PIL', filename='output/3D_resize_img_PIL.png', vmin=vmin, vmax=vmax)
 
-print(img.shape)
-print(img_resized_sk.shape)
-print(img_resized_PIL.shape)
+print("Original shape: {}".format(img.shape))
+print("skimage resized shape: {}".format(img_resized_sk.shape))
+print("PIL resized shape: {}".format(img_resized_PIL.shape))
+
+print("skimage time: {}".format(time_sk))
+print("PIL time: {}".format(time_PIL))
 
