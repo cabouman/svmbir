984966313d637b0d8d8be0ca1177e4f4f64f85ad
Soumendu Majee <soumendu.fb9@gmail.com>
2021-02-01 20:09:45 -0500

    remove skimage based image resizing from svmbir.recon while retailing skimage import statement. removing skimage import results in an installation error

--- svmbir/svmbir.py
+++ svmbir/svmbir.py
@@ -449,20 +449,13 @@ def recon_resize(recon, output_shape):
         ndarray: 3D numpy array containing interpolated reconstruction with shape (num_slices, num_rows, num_cols).
     """
 
-    if os.environ.get('RESIZE_LIB') =='SKIMAGE':
-        recon = np.transpose(recon, (1, 2, 0))
-        recon = resize(recon, output_shape)
-        recon = np.transpose(recon, (2, 0, 1))
-    else:
-        recon_resized_list = []
-        for i in range(recon.shape[0]):
-            PIL_image = PIL.Image.fromarray(recon[i])
-            PIL_image_resized = PIL_image.resize((output_shape[1],output_shape[0]), resample=PIL.Image.BILINEAR)
-            recon_resized_list.append(np.asarray(PIL_image_resized))
+    recon_resized_list = []
+    for i in range(recon.shape[0]):
+        PIL_image = PIL.Image.fromarray(recon[i])
+        PIL_image_resized = PIL_image.resize((output_shape[1],output_shape[0]), resample=PIL.Image.BILINEAR)
+        recon_resized_list.append(np.asarray(PIL_image_resized))
 
-        recon = np.stack(recon_resized_list, axis=0)
-
-    return recon
+    return np.stack(recon_resized_list, axis=0)
 
 
 def _sino_indicator(sino):
