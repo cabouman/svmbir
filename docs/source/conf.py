d827bb7f450c0a8db94a70666c12d8cdc8499e34
Charles Bouman <Charles.Bouman@gmail.com>
2021-07-21 17:04:20 -0400

    Fix requirement

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,6 @@ release = '0.2'
 extensions = [
     'IPython.sphinxext.ipython_console_highlighting',
     'IPython.sphinxext.ipython_directive',
-    'nbsphinx',
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.todo',
