69842a85baad2fd9fdc4378c078156ec6af26ed5
dyang37 <yang1467@purdue.edu>
2021-10-06 22:08:38 -0400

    move makedirs up so that output dir is created before making any plots. Add print message to print out id for angles

--- demo/demo_sino_sorting.py
+++ demo/demo_sino_sorting.py
@@ -33,6 +33,9 @@ phantom = svmbir.phantom.gen_shepp_logan_3d(num_rows_cols, num_rows_cols, num_sl
 # Generate array of view angles form 0 to 180 degs
 angles_original = np.linspace(0, np.pi, num_views, endpoint=False)
 
+# create output folder
+os.makedirs('output', exist_ok=True)
+
 # Set up plot to show angle orders for different approaches
 plt.title('Angle orders for different approaches')
 plt.xlabel('Order')
@@ -48,12 +51,9 @@ weights_original = svmbir.calc_weights(sino_original, weight_type='transmission'
 (num_views, num_slices, num_channels) = sino_original.shape
 
 # Perform fixed resolution MBIR reconstruction with original data
-start_original = time.process_time()
+start_original = time.time()
 recon_original = svmbir.recon(sino_original, angles_original, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
-end_original = time.process_time()
-
-# Compute Normalized Root Mean Squared Error with original data
-nrmse_original = svmbir.phantom.nrmse(recon_original, phantom)
+end_original = time.time()
 
 # Shuffle the angles and corresponding data
 idx = np.arange(0, num_views, 1)
@@ -61,65 +61,56 @@ np.random.shuffle(idx)
 angles_shuffled = angles_original[idx]
 sino_shuffled = sino_original[idx]
 weights_shuffled = weights_original[idx]
+print('Shuffled angles object id: ', id(angles_shuffled))
 print('Shuffled sinogram object id: ', id(sino_shuffled))
 print('Shuffled weights object id: ', id(weights_shuffled))
 plt.plot(angles_shuffled, label='Shuffled')
 
 # Perform fixed resolution MBIR reconstruction with shuffled data
-start_shuffled = time.process_time()
+start_shuffled = time.time()
 recon_shuffled = svmbir.recon(sino_shuffled, angles_shuffled, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
-end_shuffled = time.process_time()
-
-# Compute Normalized Root Mean Squared Error with shuffled data
-nrmse_shuffled = svmbir.phantom.nrmse(recon_shuffled, phantom)
+end_shuffled = time.time()
 
 # Sort the angles and corresponding data to ascending order
 sino_sorted, angles_sorted, weights_sorted = svmbir.sino_sort(sino_shuffled, angles_shuffled, weights_shuffled)
+print('Sorted angles object id: ', id(angles_sorted))
 print('Sorted sinogram object id: ', id(sino_sorted))
 print('Sorted weights object id: ', id(weights_sorted))
 plt.plot(angles_sorted, label='Sorted')
 
 # Perform fixed resolution MBIR reconstruction with sorted data
-start_sorted = time.process_time()
+start_sorted = time.time()
 recon_sorted = svmbir.recon(sino_sorted, angles_sorted, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
-end_sorted = time.process_time()
-
-# Compute Normalized Root Mean Squared Error with sorted data
-nrmse_sorted = svmbir.phantom.nrmse(recon_sorted, phantom)
-
+end_sorted = time.time()
 
 # Plot angle orders for different approaches
 plt.legend()
 plt.savefig('output/angle_orders.png')
-plt.show()
 input("press Enter to continue")
-
-# create output folder
-os.makedirs('output', exist_ok=True)
 
 # display phantom
 title = f'Slice {display_slice:d} of 3D Shepp Logan Phantom.'
 plot_image(phantom[display_slice], title=title, filename='output/3d_shepp_logan_phantom.png', vmin=vmin, vmax=vmax)
 
 # display fix resolution reconstruction with original data
-title = f'Slice {display_slice:d} of 3D Recon of the original data with NRMSE={nrmse_original:.3f}.'
+title = f'Slice {display_slice:d} of 3D Recon of the original data.'
 plot_image(recon_original[display_slice], title=title, filename='output/3d_shepp_logan_recon_original.png', vmin=vmin, vmax=vmax)
 
 # display fix resolution reconstruction with shuffled data
-title = f'Slice {display_slice:d} of 3D Recon of the shuffled data with NRMSE={nrmse_shuffled:.3f}.'
+title = f'Slice {display_slice:d} of 3D Recon of the shuffled data.'
 plot_image(recon_shuffled[display_slice], title=title, filename='output/3d_shepp_logan_recon_shuffled.png', vmin=vmin, vmax=vmax)
 
 # display fix resolution reconstruction with sorted data
-title = f'Slice {display_slice:d} of 3D Recon of the sorted data with NRMSE={nrmse_sorted:.3f}.'
+title = f'Slice {display_slice:d} of 3D Recon of the sorted data.'
 plot_image(recon_sorted[display_slice], title=title, filename='output/3d_shepp_logan_recon_sorted.png', vmin=vmin, vmax=vmax)
 
 # display diff image of reconstructions using original and shuffled sinogram
-title = f'Slice {display_slice:d} of diff image between reconstruction with original sinogram and reconstruction with shuffled sinogram.'
+title = f'Slice {display_slice:d} of Diff image between reconstructions with original and shuffled sinogram.'
 diff_orig_shuffled = recon_original - recon_shuffled
 plot_image(diff_orig_shuffled[display_slice], title=title, filename='output/diff_recon_orig_shuffled.png', vmin=-vmax/100., vmax=vmax/100.)
 
 # display diff image of reconstructions using shuffled and sorted sinogram
-title = f'Slice {display_slice:d} of diff image between reconstruction with shuffled sinogram and reconstruction with sorted sinogram.'
+title = f'Slice {display_slice:d} of Diff image between reconstructions with shuffled and sorted sinogram.'
 diff_shuffled_sorted = recon_shuffled - recon_sorted
 plot_image(diff_shuffled_sorted[display_slice], title=title, filename='output/diff_recon_orig_sorted.png', vmin=-vmax/100., vmax=vmax/100.)
 
