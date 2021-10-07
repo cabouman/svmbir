f82de4625239dd91df9e5bfc5642eb0b1b944675
dyang37 <yang1467@purdue.edu>
2021-10-06 21:18:04 -0400

    This commit contains several changes to the demo script of sino_sort function demo_sino_sort.py: 1. Changed the original angles range from [-90.90) deg to [0,180) deg. This is to make sure that the original and sorted sinograms are directly comparable. For the case where angles are from [-90.90), the sinogram view order after sorting actually corresponds to the order [0,90),[-90,0) instead of [-90,90).
    2. calculated transmission weights to test sino_sort for the weights array. Note that the weights are not used for actual reconstruction, since for the shepp-logan example using transmission weight will cause MBIR recon to never converge.
    
    3. As requested by Prof. Buzzard, added print message to quantitatively compare the NRMSE between reconstructions.
    
    4. Added diff image plots between three reconstructions
    
    5. Modified code based on Jordan's feedback.
    
    6. Added print message to display NRMSE between original and sorted sinograms.
    
    7. Change variable names so that they make more sense.

--- demo/demo_sino_sorting.py
+++ demo/demo_sino_sorting.py
@@ -16,7 +16,6 @@ display_slice = 12 # Display slice at z=-0.25
 
 # Simulated sinogram parameters
 num_views = 144
-tilt_angle = np.pi/2 # Tilt range of +-90deg
 
 # Reconstruction parameters
 T = 0.1
@@ -31,25 +30,26 @@ vmax = 1.2
 # Generate phantom
 phantom = svmbir.phantom.gen_shepp_logan_3d(num_rows_cols, num_rows_cols, num_slices)
 
-# Generate array of view angles form -90 to 90 degs
-angles = np.linspace(-tilt_angle, tilt_angle, num_views, endpoint=False)
+# Generate array of view angles form 0 to 180 degs
+angles_original = np.linspace(0, np.pi, num_views, endpoint=False)
 
 # Set up plot to show angle orders for different approaches
 plt.title('Angle orders for different approaches')
 plt.xlabel('Order')
 plt.ylabel('Angle (Rad)')
-plt.plot(angles, label='Original')
+plt.plot(angles_original, label='Original')
 
 # Generate sinogram by projecting phantom
-sino = svmbir.project(angles, phantom, num_rows_cols)
-print('Original sinogram memory address: ', id(sino))
+sino_original = svmbir.project(phantom, angles_original, num_rows_cols)
+
+weights_original = svmbir.calc_weights(sino_original, weight_type='transmission')
 
 # Determine resulting number of views, slices, and channels
-(num_views, num_slices, num_channels) = sino.shape
+(num_views, num_slices, num_channels) = sino_original.shape
 
 # Perform fixed resolution MBIR reconstruction with original data
 start_original = time.process_time()
-recon_original = svmbir.recon(sino, angles, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
+recon_original = svmbir.recon(sino_original, angles_original, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
 end_original = time.process_time()
 
 # Compute Normalized Root Mean Squared Error with original data
@@ -58,27 +58,30 @@ nrmse_original = svmbir.phantom.nrmse(recon_original, phantom)
 # Shuffle the angles and corresponding data
 idx = np.arange(0, num_views, 1)
 np.random.shuffle(idx)
-angles = angles[idx]
-sino = sino[idx]
-print('Shuffled sinogram memory address: ', id(sino))
-plt.plot(angles, label='Shuffled')
+angles_shuffled = angles_original[idx]
+sino_shuffled = sino_original[idx]
+weights_shuffled = weights_original[idx]
+print('Shuffled sinogram object id: ', id(sino_shuffled))
+print('Shuffled weights object id: ', id(weights_shuffled))
+plt.plot(angles_shuffled, label='Shuffled')
 
 # Perform fixed resolution MBIR reconstruction with shuffled data
 start_shuffled = time.process_time()
-recon_shuffled = svmbir.recon(sino, angles, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
+recon_shuffled = svmbir.recon(sino_shuffled, angles_shuffled, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
 end_shuffled = time.process_time()
 
 # Compute Normalized Root Mean Squared Error with shuffled data
 nrmse_shuffled = svmbir.phantom.nrmse(recon_shuffled, phantom)
 
 # Sort the angles and corresponding data to ascending order
-sino, angles, weights = svmbir.sino_sort(sino, angles)
-print('Sorted sinogram memory address: ', id(sino))
-plt.plot(angles, label='Sorted')
+sino_sorted, angles_sorted, weights_sorted = svmbir.sino_sort(sino_shuffled, angles_shuffled, weights_shuffled)
+print('Sorted sinogram object id: ', id(sino_sorted))
+print('Sorted weights object id: ', id(weights_sorted))
+plt.plot(angles_sorted, label='Sorted')
 
 # Perform fixed resolution MBIR reconstruction with sorted data
 start_sorted = time.process_time()
-recon_sorted = svmbir.recon(sino, angles, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
+recon_sorted = svmbir.recon(sino_sorted, angles_sorted, T=T, p=p, sharpness=sharpness, snr_db=snr_db)
 end_sorted = time.process_time()
 
 # Compute Normalized Root Mean Squared Error with sorted data
@@ -89,6 +92,7 @@ nrmse_sorted = svmbir.phantom.nrmse(recon_sorted, phantom)
 plt.legend()
 plt.savefig('output/angle_orders.png')
 plt.show()
+input("press Enter to continue")
 
 # create output folder
 os.makedirs('output', exist_ok=True)
@@ -109,9 +113,27 @@ plot_image(recon_shuffled[display_slice], title=title, filename='output/3d_shepp
 title = f'Slice {display_slice:d} of 3D Recon of the sorted data with NRMSE={nrmse_sorted:.3f}.'
 plot_image(recon_sorted[display_slice], title=title, filename='output/3d_shepp_logan_recon_sorted.png', vmin=vmin, vmax=vmax)
 
+# display diff image of reconstructions using original and shuffled sinogram
+title = f'Slice {display_slice:d} of diff image between reconstruction with original sinogram and reconstruction with shuffled sinogram.'
+diff_orig_shuffled = recon_original - recon_shuffled
+plot_image(diff_orig_shuffled[display_slice], title=title, filename='output/diff_recon_orig_shuffled.png', vmin=-vmax/100., vmax=vmax/100.)
+
+# display diff image of reconstructions using shuffled and sorted sinogram
+title = f'Slice {display_slice:d} of diff image between reconstruction with shuffled sinogram and reconstruction with sorted sinogram.'
+diff_shuffled_sorted = recon_shuffled - recon_sorted
+plot_image(diff_shuffled_sorted[display_slice], title=title, filename='output/diff_recon_orig_sorted.png', vmin=-vmax/100., vmax=vmax/100.)
+
+# display reconstruction time
 print('Processing time for original: ', end_original-start_original, 's')
 print('Processing time for shuffled: ', end_shuffled-start_shuffled, 's')
 print('Processing time for sorted: ', end_sorted-start_sorted, 's')
+
+# display nrmse of recon and weights before and after sorting
+print('NRMSE between original and shuffled sinogram = ', svmbir.phantom.nrmse(sino_original, sino_sorted))
+print('NRMSE between reconstructions using original and shuffled sinogram = ', svmbir.phantom.nrmse(recon_original, recon_shuffled))
+print('NRMSE between reconstructions using shuffled and sorted sinogram = ', svmbir.phantom.nrmse(recon_shuffled, recon_sorted))
+print('NRMSE between original weights and sorted weights = ', svmbir.phantom.nrmse(weights_original, weights_sorted))
+
 
 input("press Enter")
 
