8bb8e08c08fcbe5ca7ce3f50c38fbcc4570994bd
Charles Bouman <Charles.Bouman@gmail.com>
2021-11-09 18:43:49 -0500

    Update scripts

--- dev_scripts/clean_install_conda_svmbir_docs.sh
+++ dev_scripts/clean_install_conda_svmbir_docs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# This script destroys the conda environment named "svmbir" and uninstalls SVMBIR.
-# It then creates an "svmbir" environment and reinstalls SVMBIR along with the documentation and demo requirements.
+# This script destroys the conda environment named "svmbir" and uninstalls svmbir.
+# It then creates an "svmbir" environment and reinstalls svmbir along with the documentation and demo requirements.
 
 # Clean out old installation
 source clean_svmbir.sh
 
 # Destroy conda environement named svmbir and reinstall it
-source reinstall_conda_environment.sh
+source install_conda_environment.sh
 
 # Install svmbir
 source install_svmbir.sh
