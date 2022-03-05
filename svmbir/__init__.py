94c0b06b1a0508992239df1cacc79430f3f4c1c2
Sherman J. Kisner <sjkisner@gmail.com>
2022-03-05 17:05:54 -0500

    Update svmbir.__version__ to 0.2.8

--- svmbir/__init__.py
+++ svmbir/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.7'
+__version__ = '0.2.8'
 from .svmbir import *
 from .phantom import *
 __all__ = ['_svmbir_lib_path','_clear_cache','sino_sort','auto_sigma_x','auto_sigma_p','auto_sigma_y', 'calc_weights','project','backproject','recon']
